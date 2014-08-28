d1355f69cb15f28d3c6160e4306b4ecb7636d889
Noelia Cabane <noeliacabane@gmail.com>
2014-08-27 21:43:09 -0300

    Add DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+questions = Question.create!([
+    { text: 'Pregunta1', option_1: "P1O1", option_2: "P1O2", option_3: "P1O3" },
+    {text: 'Pregunta2', option_1: "P1O1", option_2: "P1O2", option_3: "P1O3" },
+    {text: 'Pregunta3', option_1: "P1O1", option_2: "P1O2", option_3: "P1O3" }
+  ])
